a1330923d3d86078fa6542c0213c2442b177d1b6
danielxurbina <danielur1999@gmail.com>
2020-05-24 17:47:41 -0400

    added filter and sort

--- src/containers/Login.js
+++ src/containers/Login.js
@@ -1,29 +1,25 @@
 import React from 'react'
 
 class Login extends React.Component {
-    state = { username: "", password: ""}
-
-    handleLogin = (event) => {
-        this.setState({[event.target.name]: event.target.value})
-    }
-
     render(){
         return(
             <div>
                 Sign In
-                <form onSubmit={() => this.props.loginSubmitHandler(this.state.username)}>
+                <form >
                 <label>
                     Username:
-                    <input type="text" name="username" value={this.state.username} onChange={(event) => this.handleLogin(event)} />
+                    <input type="text" name="username" value={""} onChange={(event) => this.props.loginHandler(event)} />
                 </label>
                 <label>
                     Password: 
-                    <input name="password" placeholder="Password" type="password" value={this.state.password} onChange={(event) => this.handleLogin(event)}/>
+                    <input name="password" placeholder="Password" type="password" value={""} onChange={(event) => this.props.loginHandler(event)}/>
                 </label>
                     <input type="submit" value="Submit" />
             </form>
             </div>
-       
+            // <div>
+            //     Sign in
+            // </div>
         )
     }
 }
