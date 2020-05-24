bde3c2122fdac2dfd20a891df0ce92fae49f5069
danielxurbina <danielur1999@gmail.com>
2020-05-24 12:10:17 -0400

    added solution to App.js

--- src/containers/Login.js
+++ src/containers/Login.js
@@ -1,15 +1,31 @@
 import React from 'react'
 
-const Login = () => {
-return(
-    <div>
-        Log In
-        <div>
-            testing
-        </div>
-    </div>
-   
-)
+class Login extends React.Component {
+    state = { username: "", password: ""}
+
+    handleLogin = (event) => {
+        this.setState({[event.target.name]: event.target.value})
+    }
+
+    render(){
+        return(
+            <div>
+                Sign In
+                <form onSubmit={() => this.props.loginSubmitHandler(this.state.username)}>
+                <label>
+                    Username:
+                    <input type="text" name="username" value={this.state.username} onChange={(event) => this.handleLogin(event)} />
+                </label>
+                <label>
+                    Password: 
+                    <input name="password" placeholder="Password" type="password" value={this.state.password} onChange={(event) => this.handleLogin(event)}/>
+                </label>
+                    <input type="submit" value="Submit" />
+            </form>
+            </div>
+       
+        )
+    }
 }
 
 export default Login
