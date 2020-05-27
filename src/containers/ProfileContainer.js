51a492799dd9465c4b791a5ed1e24f06e2f205be
Kelsbecker <kelly.lynn.becker@gmail.com>
2020-05-27 19:17:48 -0400

    edited user persisting and rendering

--- src/containers/ProfileContainer.js
+++ src/containers/ProfileContainer.js
@@ -16,17 +16,11 @@ class ProfileContainer extends React.Component {
         image_url: ""
     }
 
-    // this will match the users id when you click on the visit button from the homepage and render the page that belongs to that user 
     componentDidMount(){
-        // fetch(`http://localhost:3000/users/${this.props.match.params.id}`)
-        // .then(response => response.json())
-        // .then(user => this.setState({user: user}))
         this.setState({id: this.props.currentUser.id, name: this.props.currentUser.attributes.name, username: this.props.currentUser.attributes.username,
                       bio: this.props.currentUser.attributes.bio, image_url: this.props.currentUser.attributes.image_url
                     })
     }
-
-    
 
     toggleForm = () => {
         this.setState({
@@ -40,7 +34,7 @@ class ProfileContainer extends React.Component {
         })
     }
 
-    handleSubmit = (event) => { //getting 404 need update method in backend
+    handleSubmit = (event) => {
         event.preventDefault()
         let editedUser = {
             name: this.state.name,
@@ -48,22 +42,15 @@ class ProfileContainer extends React.Component {
             image_url: this.state.image_url,
             username: this.state.username
         }
-        console.log(editedUser)
         fetch(`http://localhost:3000/users/${parseInt(this.state.id)}`, {
             method: 'PATCH',
             headers: header,
             body: JSON.stringify(editedUser)
         })
         .then(response => response.json())
-        .then(user => this.props.updateCurrentUser(user)
-        // .then(updatedUser =>  this.setState({
-        //     name: updatedUser.data.attributes.name, 
-        //     username: updatedUser.data.attributes.username, 
-        //     bio: updatedUser.data.attributes.bio, 
-        //     image_url: updatedUser.data.attributes.image_url
-        // }))
-        )
+        .then(user => this.props.updateCurrentUser(user))
     }
+    
 
     renderUser = () => {
             const{image_url, name, username, bio} = this.props.currentUser.attributes
@@ -80,7 +67,7 @@ class ProfileContainer extends React.Component {
 
     renderEditForm = () => {
         return(
-            <form onSubmit={this.handleSubmit}>
+            <form onSubmit={(event) => this.handleSubmit(event)}>
                 <label>Edit Profile Info:
                 <input type='text' name='name' placeholder='Change Name' value={this.state.name} onChange={this.handleFormChange}></input>
                 <input type='text' name='username' placeholder='Change Username' value={this.state.username} onChange={this.handleFormChange}></input>
