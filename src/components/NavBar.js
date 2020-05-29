ca8e7aab879f1c8c7c5d3caf8a53580a1ddfbe77
Kelsbecker <kelly.lynn.becker@gmail.com>
2020-05-28 20:23:11 -0400

    navbar spacing

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,26 +5,26 @@ import {Navbar, NavbarBrand, Nav, NavItem} from 'reactstrap';
 class NavBar extends React.Component {
     render() { 
         return ( 
-            <Navbar color="transparent" light expand="md">
-                <NavbarBrand to="/">Locali</NavbarBrand>
+            <Navbar color="transparent" light expand="md" bg="light">
+                <NavbarBrand to="/" style={{fontSize: 35}}>Locali</NavbarBrand>
                 <Nav className="mr-auto" navbar>
                     <NavItem>
-                        <NavLink to="/login">Log In</NavLink>
+                        <NavLink to="/login" style={{marginRight: 10}}>Log In</NavLink>
                     </NavItem>
                     <NavItem>
-                        <NavLink to="/signup">Sign Up</NavLink>
+                        <NavLink to="/signup" style={{marginRight: 10}}>Sign Up</NavLink>
                     </NavItem>
                     <NavItem>
-                        <NavLink to="/homepage">Homepage</NavLink>
+                        <NavLink to="/homepage" style={{marginRight: 10}}>Homepage</NavLink>
                     </NavItem>
                     <NavItem>
-                        <NavLink to="/events">Events</NavLink>
+                        <NavLink to="/events" style={{marginRight: 10}}>Events</NavLink>
                     </NavItem>
                     <NavItem>
-                        <NavLink to="/profile/:id">Profile Page</NavLink>
+                        <NavLink to="/profile/:id" style={{marginRight: 10}}>Profile Page</NavLink>
                     </NavItem>
                     <NavItem>
-                        <NavLink to="/">Sign Out</NavLink>
+                        <NavLink to="/" style={{marginRight: 10}}>Sign Out</NavLink>
                     </NavItem>
                 </Nav>
             </Navbar>
